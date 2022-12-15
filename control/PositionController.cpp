d6b9b130bc0fecf065bd913da516b052ae14c6c4
RedwanNewaz <redwan06me@egmail.com>
2022-12-15 07:03:13 -0600

    pid control

--- control/PositionController.cpp
+++ control/PositionController.cpp
@@ -7,7 +7,13 @@
 #include <utility>
 
 bebop2::PositionController::PositionController( std::shared_ptr<StateEstimation> stateEstimation) : stateEstimation_(std::move(stateEstimation)) {
-    timer_ = nh_.createTimer(ros::Duration(0.03), &bebop2::PositionController::control_loop, this);
+
+    ros::param::get("~dt", dt_);
+    ros::param::get("~goal_thres", goal_thres_);
+    std::vector<double> gains;
+    ros::param::get("~pid_gains", gains);
+    set_gains(gains, m_quadController);
+    timer_ = nh_.createTimer(ros::Duration(dt_), &bebop2::PositionController::control_loop, this);
 }
 
 void bebop2::PositionController::control_loop(const ros::TimerEvent &event) {
@@ -20,6 +26,16 @@ void bebop2::PositionController::control_loop(const ros::TimerEvent &event) {
     {
         setPoints_ = current;
         update_set_point(0, 0, 0);
+    }
+
+    if(buttonState_ == CONTROL)
+    {
+        std::vector<double>X{current.x,current.y,current.z,0}, Xg{setPoints_.x,setPoints_.y,setPoints_.z,0.0}, U(NUM_CONTROLLER);
+        if(!compute_control(X, Xg, m_quadController, U))
+        {
+            ROS_INFO("[PositionController] vx = %lf, vy = %lf, vz = %lf", U[0], U[1], U[2]);
+        }
+
     }
 
 
@@ -36,4 +52,38 @@ void bebop2::PositionController::update_set_point(double dx, double dy, double d
     tf::Transform transform;
     transform.setOrigin(tf::Vector3(setPoints_.x, setPoints_.y, setPoints_.z));
     update_setpoint_viz(transform);
+}
+
+void bebop2::PositionController::set_gains(const std::vector<double> &gains, bebop2::PID *controller) {
+    const double MAX_OUT = 1;
+    const double MIN_OUT = -1;
+
+    for (int i = 0; i < NUM_CONTROLLER; ++i) {
+        int j = i * NUM_GAINS;
+        controller[i].init(dt_, MAX_OUT, MIN_OUT, gains[j], gains[j + 1], gains[j + 2]);
+    }
+}
+
+double bebop2::PositionController::goal_distance(const std::vector<double> &X, const std::vector<double> &setPoints) {
+    double error = 0;
+    for (int i = 0; i < X.size(); ++i) {
+        double e = X[i] - setPoints[i];
+        error += e * e;
+    }
+    return sqrt(error);
+}
+
+bool bebop2::PositionController::compute_control(const std::vector<double> &X, const std::vector<double> &setPoints,
+                                                 bebop2::PID *controller, std::vector<double> &control) {
+    std::lock_guard<std::mutex> lk(mu_);
+    auto dist = goal_distance(X, setPoints);
+    if(dist < goal_thres_)
+    {
+        ROS_INFO("[PositionController]: Hovering radius = %lf", dist );
+        return false;
+    }
+    for (int i = 0; i < NUM_CONTROLLER; ++i) {
+        control[i] = controller[i].calculate(setPoints[i], X[i]);
+    }
+    return true;
 }
--- control/PositionController.h
+++ control/PositionController.h
@@ -6,6 +6,9 @@
 #define BEBOP2_CONTROLLER_POSITIONCONTROLLER_H
 #include "JoystickController.h"
 #include "../localization/StateEstimation.h"
+#include "PID/PID.h"
+#define NUM_CONTROLLER  4
+#define NUM_GAINS 3
 
 namespace bebop2
 {
@@ -17,11 +20,22 @@ namespace bebop2
         std::shared_ptr<StateEstimation> stateEstimation_;
         FieldLocation setPoints_;
         ros::Timer timer_;
+        double goal_thres_;
+        double dt_;
+        bebop2::PID m_quadController[NUM_CONTROLLER];
+
 
     protected:
         void control_loop(const ros::TimerEvent& event);
 
         void update_set_point(double dx, double dy, double dz) override;
+
+        void set_gains(const std::vector<double>& gains, bebop2::PID* controller);
+
+        double goal_distance(const std::vector<double>& X, const std::vector<double>& setPoints);
+
+        bool compute_control(const std::vector<double>& X, const std::vector<double>& setPoints, bebop2::PID* controller, std::vector<double>& control);
+
     };
 
 }

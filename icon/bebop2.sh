fd4ff8910d200989ac46db0da8d722c08d7abdf7
RedwanNewaz <redwan06me@gmail.com>
2023-03-06 09:49:07 -0600

    desktop icon added

--- icon/bebop2.sh
+++ icon/bebop2.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash 
 
+#setup the ROS environment
 LD_LIBRARY_PATH=~/catkin_ws/devel/lib/parrot_arsdk:$LD_LIBRARY_PATH &&source ~/catkin_ws/devel/setup.bash
-
+#launch roscore in separate thread
+coproc(roscore)
+#launch the main program
 rosrun bebop2_controller bebop2_controller_gui

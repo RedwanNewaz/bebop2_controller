94ebe889faa0e58ce9d14d5ad6a5d46694dc416b
Simant-Singh <103738773+Simant-Singh@users.noreply.github.com>
2023-05-15 01:07:37 -0500

    Documented mainwindow.h

--- bebop2_controller_gui/mainwindow.h
+++ bebop2_controller_gui/mainwindow.h
@@ -15,16 +15,20 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/// @brief It acts as a graphical user interface of the program. It provides users with a graphical interface to interact with the application and access its functionality.
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /// @brief Constructor for the Main Window. It initializes the UI elements and sets the initial state of the program 
+    /// @param parent used to specify the parent of the current widget
     MainWindow(QWidget *parent = nullptr);
+    /// @brief Destructor method for the MainWindow class. It cleans up any resources that were allocated by the class.
     ~MainWindow();
 
 private slots:
-    void on_btn_start_clicked();
+    /// @brief A method that sets the current state of the various checkboxes in the GUI based on the options selected by the user.    void on_btn_start_clicked();
     void timer_callback();
     void on_op_type_sim_toggled(bool checked);
 

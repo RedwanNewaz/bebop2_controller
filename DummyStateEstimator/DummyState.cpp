f59c403695a544094559a5b6d1b7a407eec507ce
airlab <aredwann@uno.edu>
2023-06-01 16:07:20 -0500

    dummy state and controller tested

---
+++ DummyStateEstimator/DummyState.cpp
@@ -0,0 +1,82 @@
+#include "DummyState.h"
+
+
+namespace bebop2
+{
+    DummyState::DummyState(ros::NodeHandle& nh, bool noisy_reading):nh_(nh), noisy_reading_(noisy_reading)
+    {
+        states_.resize(STATE_DIM);
+        std::fill(states_.begin(), states_.end(), 0.0);
+
+        queue_.push(states_);
+
+        sub_takeoff_ = nh_.subscribe("/bebop/takeoff", 10, &DummyState::takeoff_callback, this);
+        sub_land_ = nh_.subscribe("/bebop/land", 10, &DummyState::land_callback, this);
+        sub_cmd_vel_ = nh_.subscribe("/bebop/cmd_vel", 10, &DummyState::cmd_vel_callback, this);
+
+        dummy_timer_ = nh_.createTimer(ros::Duration(DT), &DummyState::updateState, this);
+        ROS_INFO("[DummyState] DummyState initialized ...");
+    }
+
+    void DummyState::operator()(std::vector<double>& state)
+    {
+        if(state.size() != STATE_DIM )
+        {
+            state.clear();
+            std::copy(states_.begin(), states_.end(), std::back_inserter(state));
+        }
+        else{
+            for (int i = 0; i < STATE_DIM; ++i)
+                state[i] = states_[i];
+        }
+
+
+//        ROS_INFO("state = (%lf %lf %lf %lf)", states_[0], states_[1], states_[2], states_[3] );
+
+        // don't add noise if noisy_reading is not enabled
+        if(!noisy_reading_)
+            return;
+
+        // add gaussian white noise for simulating a realistic measurement
+        std::normal_distribution<double> disturbance(0.0, sqrt(NOISE));
+        //focus on x, y coordinates only. Later tune it for all 4 axes
+        for(std::size_t i=0; i<state.size() - 2; ++i)
+            state[i] += disturbance(rd_);
+
+    }
+
+    bool DummyState::empty()
+    {
+        bool empty =  queue_.empty();
+        if(!empty)
+            queue_.pop();
+        return empty;
+    }
+
+    void DummyState::takeoff_callback(const std_msgs::Empty::ConstPtr& msg)
+    {
+        states_[2] = 1.0;
+        ROS_INFO("[DummyState] takeoff message received");
+    }
+    void DummyState::land_callback(const std_msgs::Empty::ConstPtr& msg)
+    {
+        states_[2] = 0.0;
+        ROS_INFO("[DummyState] land message received");
+    }
+    void DummyState::cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
+    {
+        states_[0] += DT * msg->linear.x;
+        // parrot bebop2 moves left for (+) linear.y and vice versa
+        states_[1] -= DT * msg->linear.y;
+        states_[2] += DT * msg->linear.z;
+        states_[3] += DT * msg->angular.z;
+
+        ROS_INFO("[DummyState] state = (%lf, %lf, %lf, %lf)", states_[0], states_[1], states_[2], states_[3]);
+    }
+
+    void DummyState::updateState(const ros::TimerEvent &event) {
+        queue_.push(states_);
+    }
+
+} // namespace bebop2
+
---
+++ DummyStateEstimator/DummyState.h
@@ -0,0 +1,97 @@
+#pragma once 
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <math.h>
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+#include <queue>
+#include <tf/transform_datatypes.h>
+
+namespace bebop2{
+    /** 
+     * @brief Dummy state creates a state space repersentation for quadrotor. 
+     
+     * The key idea is to initialize a state at (0, 0, 0, 0) and take joystick command to update state. 
+     * To capture realistic behavior add some gaussian white noise to the state. 
+     * When bebop2 takeoff from the ground it reaches 1m above from the ground origin and when it lands then z axis value is 0. 
+     * This behavior can be achieved by simply subscribing takeoff and land messages.
+     * To change bebop coordinates in x and y direction subscribe cmd_vel topic and update the state based on the cmd_vel value. 
+     * cmd_vel is a geometry_msg/Twist msg which provides 4 control inputs, i.e., linear.x, linear.y, linear.z, angular.z
+     * Here, we need to make a assumption that sampling time DT is a constant value.  
+     * Finally to integrate with our main framework, we need a public method void operator()(std::vector<double>& state) that can populate a requested state vector; 
+     */
+    class DummyState{
+
+    public:
+        /**
+         * @brief Construct a new Dummy State object
+         * To simulate desired behavior from a sensor so that we can develop accurate control module 
+         * @param nh ros NodeHandle to deal with various messages
+         * @param noisy_reading an option to make a realistic sensor reading: if true white noise will be added to the output 
+         */
+        DummyState(ros::NodeHandle& nh, bool noisy_reading);
+        /** @brief Populates the requested state vector.
+        *   @param state Gives the current position of the drone.
+        */
+        void operator()(std::vector<double>& state);
+        bool empty();
+
+        tf::Transform getState()
+        {
+            tf::Transform transform;
+            std::vector<double>state;
+            this->operator()(state);
+            transform.setOrigin(tf::Vector3(state[0], state[1], state[2]));
+            auto q = tf::Quaternion(0, 0, 0, 1);
+            transform.setRotation(q);
+            return transform;
+        }
+
+    private:
+        ros::NodeHandle nh_;
+        // Timer to call the updateState() method.
+        ros::Timer dummy_timer_;
+        /// @brief internal states must contain at least four vairables [x, y, z, yaw] for the position control module
+        std::vector<double> states_;
+        /// @brief bebop_autonomy messages for basic maneuvers 
+        ros::Subscriber sub_takeoff_, sub_land_, sub_cmd_vel_;
+        /// @brief to generate Gaussian white noise we need random device
+        std::random_device rd_;
+        bool noisy_reading_;
+        
+        const size_t STATE_DIM = 4;
+        /// @brief discrete sample time is determined by control frequency
+        const double DT = 0.01; // 100 Hz
+        /// @brief white noise amplitude 
+        const double NOISE = 0.2;
+        std::queue<std::vector<double>> queue_;
+    protected:
+        /**
+         * @brief Joystic command to takeoff the bebop2 
+         * @param msg  publish empty message to the topic ```takeoff```
+        */
+        void takeoff_callback(const std_msgs::Empty::ConstPtr& msg);
+        /**
+         * @brief @brief Joystic command to land the bebop2 
+         * 
+         * @param msg publish empty message to the topic ```land```
+         */
+        void land_callback(const std_msgs::Empty::ConstPtr& msg);
+        /**
+         * @brief Joystic command to move the bebop2 
+         * 
+         * @param msg publish Twist message to the topic ```cmd_vel``` 
+         */
+        void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg);
+
+        /**
+        * @brief Updates the state of the dummy drone.
+        */
+        void updateState(const ros::TimerEvent& event);
+
+    };
+}
+
